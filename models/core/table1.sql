912e7cfdf68e662b1ab048d1040262f16fdbbd8a
alexis david <alexis.david@a4media.com>
2024-06-08 11:26:37 -0400

    make sql changes

--- models/core/table1.sql
+++ models/core/table1.sql
@@ -1,6 +1,6 @@
-   SELECT
-        org_id
-        , org_name
-        , employee_range
-        , created_at
-    FROM {{ ref('org_created') }}
+--    SELECT
+--         org_id
+--         , org_name
+--         , employee_range
+--         , created_at
+--     FROM {{ ref('org_created') }}
